83e10e1be2eb75afc610a20bc73ba3de4b59f310
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-08-30 18:40:59 +0300

    prepare the list of countries needed by recyclerView

--- app/src/main/java/com/example/tokyo2020/data/DataManager.kt
+++ app/src/main/java/com/example/tokyo2020/data/DataManager.kt
@@ -4,6 +4,8 @@ import com.example.tokyo2020.data.domain.Country
 
 object DataManager {
     private val countriesList = mutableListOf<Country>()
+    val countries: List<Country>
+        get() = countriesList
 
     fun addCountry(country: Country){
         countriesList.add(country)
