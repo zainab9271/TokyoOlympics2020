f79db12ef48322af2091851adca2a8593dfee88d
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-08-31 08:45:54 +0300

    update country adapter

--- app/src/main/java/com/example/tokyo2020/ui/CountryAdapter.kt
+++ app/src/main/java/com/example/tokyo2020/ui/CountryAdapter.kt
@@ -1,13 +1,14 @@
 package com.example.tokyo2020.ui
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tokyo2020.R
 import com.example.tokyo2020.data.domain.Country
-import com.example.tokyo2020.ui.viewHolders.CountryViewHolder
+import com.example.tokyo2020.databinding.ItemCountryBinding
 
-class CountryAdapter(val list: List<Country>): RecyclerView.Adapter<CountryViewHolder>() {
+class CountryAdapter(private val list: List<Country>): RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_country, parent, false)
         return CountryViewHolder(view)
@@ -15,16 +16,20 @@ class CountryAdapter(val list: List<Country>): RecyclerView.Adapter<CountryViewH
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
         val currentCountry = list[position]
-        holder.apply {
-            textCountryRank.text = currentCountry.rank.toString()
-            imageCountryFlag.setImageResource(currentCountry.flagId)
-            textCountryCode.text = currentCountry.nocCode
-            textCountryGolds.text = currentCountry.goldMedal.toString()
-            textCountrySilvers.text = currentCountry.silverMedal.toString()
-            textCountryBronzes.text = currentCountry.bronzeMedal.toString()
-            textCountryTotal.text = currentCountry.total.toString()
+        holder.binding.apply {
+            textRank.text = currentCountry.rank.toString()
+            imageFlag.setImageResource(currentCountry.flagId)
+            textNoc.text = currentCountry.nocCode
+            textGoldMedals.text = currentCountry.goldMedal.toString()
+            textSilverMedals.text = currentCountry.silverMedal.toString()
+            textBronzeMedals.text = currentCountry.bronzeMedal.toString()
+            textTotal.text = currentCountry.total.toString()
         }
     }
 
     override fun getItemCount() = list.size
+
+    class CountryViewHolder(viewItem: View): RecyclerView.ViewHolder(viewItem) {
+        val binding = ItemCountryBinding.bind(viewItem)
+    }
 }
